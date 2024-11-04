e756a260b0152c08c4f61dd519623b8e3b32a29f
NirForster <162992599+NirForster@users.noreply.github.com>
2024-11-04 17:51:44 +0200

    added jokesDummyData

---
+++ Client/SRC/jokesDummyData.js
@@ -0,0 +1,40 @@
+[
+  {
+    content: "Why did the bicycle fall over? It was two-tired!",
+    category: "Pun",
+    createdAt: "2024-11-04T10:30:00Z",
+    user: "645c1eab57f9bc7e8f9b1234",
+  },
+  {
+    content: "I'm reading a book on anti-gravity. It's impossible to put down!",
+    category: "Dad Joke",
+    createdAt: "2024-11-04T11:15:00Z",
+    user: "645c1eab57f9bc7e8f9b5678",
+  },
+  {
+    content: "Knock knock.\nWho's there?\nAtch.\nAtch who?\nBless you!",
+    category: "Knock-Knock",
+    createdAt: "2024-11-04T12:45:00Z",
+    user: "645c1eab57f9bc7e8f9b9012",
+  },
+  {
+    content:
+      "Why don’t scientists trust atoms? Because they make up everything!",
+    category: "Pun",
+    createdAt: "2024-11-04T13:30:00Z",
+    user: "645c1eab57f9bc7e8f9b1234",
+  },
+  {
+    content:
+      "Knock knock.\nWho's there?\nOlive.\nOlive who?\nOlive you and I miss you!",
+    category: "Knock-Knock",
+    createdAt: "2024-11-04T14:00:00Z",
+    user: "645c1eab57f9bc7e8f9b5678",
+  },
+  {
+    content: "How does a penguin build its house? Igloos it together!",
+    category: "Dad Joke",
+    createdAt: "2024-11-04T14:30:00Z",
+    user: "645c1eab57f9bc7e8f9b9012",
+  },
+];
