4d15da803bf5cb517a3806c02947f8072b39fcc3
Aharon Cohen <ayc@live.co.uk>
2024-11-05 16:41:36 +0200

    wefoi

--- Client/SRC/model.js
+++ Client/SRC/model.js
@@ -1,1 +1,7 @@
-// handle api requests
+async function fetchAllJokes(params) {
+  try {
+    const response = await fetch("/api/jokes/all");
+    const jokes = await response.json();
+    return jokes;
+  } catch (error) {}
+}
