8425a54ca12be98e0bd5274ae59b96151591b432
Aharon Cohen <ayc@live.co.uk>
2024-11-05 14:30:06 +0200

    update?

--- Client/SRC/model.js
+++
@@ -1,7 +0,0 @@
-import jokesArray from "./jokesDummyData.js";
-const tempId = "645c1eab57f9bc7e8f9b1234";
-function getJokesByUserId(tempId) {
-  return jokesArray.filter((joke) => joke.user === tempId);
-}
-
-console.log(getJokesByUserId(tempId));
